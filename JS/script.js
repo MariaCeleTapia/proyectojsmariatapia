be0ff7ea2540734c3de859a6626d2a8122e9b42c
“MariaCeleTapia” <marice.tap16@gmail.com>
2023-04-11 22:33:45 -0600

    mdificando

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+function convertir() {
+    var valore = parseInt(document.getElementById("valor").value);
+    var resultado = 0;
+    var Pesos Arg = 214;
+    var Dolar = 1;
+    if (document.getElementById("uno").checked){
+        resultado = valore / 
+    }
+
+}
